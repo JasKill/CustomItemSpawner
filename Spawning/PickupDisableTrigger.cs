9069fc8192f73af68b9f51dad1c502ea583cffaa
Arifloof <dustswier@gmail.com>
2020-08-11 04:35:15 -0600

    v2.0 beta Can edit player spawns.

--- Spawning/PickupDisableTrigger.cs
+++ Spawning/PickupDisableTrigger.cs
@@ -1,12 +1,23 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace ArithFeather.CustomItemSpawner.Spawning {
-	public class PickupDisableTrigger : MonoBehaviour
-	{
+	public class PickupDisableTrigger : MonoBehaviour {
 		private ItemSpawnPoint _itemSpawnPoint;
+
+		public List<PickupDisableTrigger> Copies = new List<PickupDisableTrigger>();
 
 		public void Initialize(ItemSpawnPoint itemSpawnPoint) => _itemSpawnPoint = itemSpawnPoint;
 
-		public void PickedUp() => _itemSpawnPoint.SetFree();
+		public void PickedUp() {
+			_itemSpawnPoint?.SetFree();
+
+			var copyCount = Copies.Count;
+			for (int i = 0; i < copyCount; i++) {
+				Destroy(Copies[i]);
+			}
+		}
+
+		public void KillTrigger() => _itemSpawnPoint = null;
 	}
 }
--- Spawning/Spawner.cs
+++ Spawning/Spawner.cs
@@ -46,15 +46,18 @@ namespace ArithFeather.CustomItemSpawner.Spawning {
 		#region Static spawning methods
 
 		private static Inventory _cachedInventory;
-		public static void SpawnItem(ItemSpawnPoint point, ItemData itemType) {
-			if (itemType.Item == ItemType.None) return;
+		public static PickupDisableTrigger SpawnItem(ItemSpawnPoint point, ItemData itemType) {
+			if (itemType.Item == ItemType.None) return null;
 
 			var pickup = _cachedInventory.SetPickup(itemType.Item, -4.65664672E+11f, point.Position, point.Rotation, 0, 0, 0);
 
 			if (CustomItemSpawner.Configs.EnableItemTracking) {
 				var listener = pickup.gameObject.AddComponent<PickupDisableTrigger>();
 				listener.Initialize(point);
+				return listener;
 			}
+
+			return null;
 		}
 
 		public static void SpawnItems(List<SpawnInfo> spawns) => Timing.RunCoroutine(StaggerSpawnItems(spawns), Segment.FixedUpdate);
@@ -65,9 +68,26 @@ namespace ArithFeather.CustomItemSpawner.Spawning {
 				var spawn = spawns[i];
 				var copies = spawn.ItemData.Copies;
 
+				var copyList = new List<PickupDisableTrigger>(copies);
+
 				for (int j = 0; j < copies; j++) {
-					SpawnItem(spawn.ItemSpawnPoint, spawn.ItemData);
+					copyList.Add(SpawnItem(spawn.ItemSpawnPoint, spawn.ItemData));
 					yield return Timing.WaitForOneFrame;
+				}
+
+				for (int j = 0; j < copies; j++)
+				{
+					var trig1 = copyList[j];
+
+					for (int k = 0; k < copies; k++)
+					{
+						var trig2 = copyList[k];
+
+						if (j != k)
+						{
+							trig1.Copies.Add(trig2);
+						}
+					}
 				}
 			}
 		}
